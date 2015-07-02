e6d8daae0d93bd8ce6463a728b860f002ccd5108
sooham <rafizsooham@gmail.com>
2015-07-02 13:45:53 -0400

    Added electonic life better

--- Eloquent Javascript/07/electronic_life.js
+++ Eloquent Javascript/07/electronic_life.js
@@ -1,16 +1,14 @@
-//  World - turn, toString
-//      plan - array of strings with one char per square
+// Plan array of strings with legends
+// World - toString - see the state
+//       - turn - take turns
 
-
-// representing space
-// use vectors to represent coord pairs
-// use a grid to make the board
-function Vector(x, y) {
+function Vector (x, y) {
     this.x = x;
     this.y = y;
 }
+
 Vector.prototype.plus = function(other) {
-    return Vector(this.x + other.x, this.y + other.y);
+    return new Vector(this.x + other.x, this.y + other.y);
 };
 
 function Grid(width, height) {
@@ -18,35 +16,39 @@ function Grid(width, height) {
     this.width = width;
     this.height = height;
 }
+
 Grid.prototype.isInside = function(vector) {
     return vector.x >= 0 && vector.x < this.width &&
-           vector.y >- 0 && vector.y < this.height;
+           vector.y >= 0 && vector.y < this.height;
 };
+
 Grid.prototype.get = function(vector) {
     return this.space[vector.x + this.width * vector.y];
 };
+
 Grid.prototype.set = function(vector, value) {
     this.space[vector.x + this.width * vector.y] = value;
 };
 
-// critter programming interface
-//      act method - returns an action
-//        action - type and other info
-// the act method takes in a view - allows critter to inspect around
+// Critter - act (takes in view)- returns critter's action this turn
+// action - has a type property and extra info
+// view
 
+// map from direction name to coordinate offsets
 var directions = {
-    "n":  new Vector( 0, -1),
-    "ne": new Vector( 1, -1),
-    "e":  new Vector( 1,  0),
-    "se": new Vector( 1,  1),
-    "s":  new Vector( 0,  1),
-    "sw": new Vector(-1,  1),
-    "w":  new Vector(-1,  0),
+    "n": new Vector(0, -1),
+    "ne": new Vector(1, -1),
+    "e": new Vector(1, 0),
+    "se": new Vector(1, 1),
+    "s": new Vector(0, 1),
+    "sw": new Vector(-1, 1),
+    "w": new Vector(-1, 0),
     "nw": new Vector(-1, -1)
 };
 
-// the view object
-// method - look - takes direction and return a character
+// view - look - takes direction and returns legend
+// view - find - takes in legend as argument, returns direction in which character can be found
+// view - findAll - returns Array containing all directions with vector
 
 function randomElement(array) {
     return array[Math.floor(Math.random() * array.length)];
@@ -56,16 +58,17 @@ var directionNames = "n ne e se s sw w nw".split(" ");
 
 function BouncingCritter() {
     this.direction = randomElement(directionNames);
-};
+}
 
 BouncingCritter.prototype.act = function(view) {
-    if (view.look(this.direction) != " ")
+    if(view.look(this.direction) != " ")
         this.direction = view.find(" ") || "s";
     return {type: "move", direction: this.direction};
 };
 
-// World object - takes in a plan (the array of strings making the world grid)
-// and legend which maps char to constructors
+
+// making the world object constructor
+// legend - object that tells us what each object means in grid
 
 function elementFromChar(legend, ch) {
     if (ch == " ")
@@ -77,17 +80,17 @@ function elementFromChar(legend, ch) {
 
 function World(map, legend) {
     var grid = new Grid(map[0].length, map.length);
-    this.grid = grid
+    this.grid = grid;
     this.legend = legend;
 
     map.forEach(function(line, y) {
-        for (var x = 0; x <line.length; x++)
-            grid.set(new Vector(x, y), elementFrom(legend, line[x]));
+        for (var x = 0; x < line.length; x++)
+            grid.set(new Vector(x, y), elementFromChar(legend, line[x]));
     });
 }
 
 function charFromElement(element) {
-    if (element == null)
+    if (element === null)
         return " ";
     else
         return element.originChar;
@@ -105,9 +108,23 @@ World.prototype.toString = function() {
     return output;
 };
 
+// defining objects to be used in legends
 function Wall() {}
 
-World.prototype.turn - function() {
+// define a forEach for grid
+Grid.prototype.forEach = function(f, context) {
+    for (var y = 0; y < this.height; y++) {
+        for (var x = 0; x < this.width; x++) {
+            var value = this.space[x + y * this.width];
+            if (value !== null)
+                f.call(context, value, new Vector(x, y));
+        }
+    }
+};
+
+// Animating life
+// Writing a turn method for the world object
+World.prototype.turn  = function() {
     var acted = [];
     this.grid.forEach(function(critter, vector) {
         if (critter.act && acted.indexOf(critter) == -1) {
@@ -121,17 +138,17 @@ World.prototype.letAct = function(critter, vector) {
     var action = critter.act(new View(this, vector));
     if (action && action.type == "move") {
         var dest = this.checkDestination(action, vector);
-        if (dest && this.grid.set(dest) == null) {
+        if (dest && this.grid.get(dest) === null) {
             this.grid.set(vector, null);
-            this.grif.set(dest, critter);
+            this.grid.set(dest, critter);
         }
     }
 };
 
 World.prototype.checkDestination = function(action, vector) {
-    if (directions.hasOwnProperty(action.direction)) {
+    if (direction.hasOwnProperty(action.direction)) {
         var dest = vector.plus(directions[action.direction]);
-        if (this.grid.isInsideA)
+        if (this.grid.idInside(dest))
             return dest;
     }
 };
@@ -142,7 +159,7 @@ function View(world, vector) {
 }
 
 View.prototype.look = function(dir) {
-    var target = this.vector.plus(directions[dir]);
+    var targer = this.vector.plus(directions[dir]);
     if (this.world.grid.isInside(target))
         return charFromElement(this.world.grid.get(target));
     else
@@ -151,17 +168,14 @@ View.prototype.look = function(dir) {
 
 View.prototype.findAll = function(ch) {
     var found = [];
-    for (var dir in directions)
+    for (var dir in firections)
         if (this.look(dir) == ch)
-            found.push(dir)
+            found.push(dir);
     return found;
 };
 
 View.prototype.find = function(ch) {
     var found = this.findAll(ch);
-    if (found.length == 0) return null;
+    if (found.length === 0) return null;
     return randomElement(found);
 };
-
-
-
