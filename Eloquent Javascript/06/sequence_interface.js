f418d62a00e6af05dc221304c8da6d5e43df8551
sooham <rafizsooham@gmail.com>
2015-07-02 11:24:39 -0400

    Finished chapter 6 exercise prototype sequence

--- Eloquent Javascript/06/sequence_interface.js
+++ Eloquent Javascript/06/sequence_interface.js
@@ -5,3 +5,28 @@
  * it is made up of and having some way to find out when the end of the
  * sequence is reached.
  */
+
+// define the Node constructor
+function Node(key, value, next)  {
+    this.key = key;
+    this.value = value;
+    this.nextNode = next || undefined;
+}
+
+// A sequence is a collection of zero or more Nodes
+
+function Sequence(head) {
+    // Creates a sequence starting at head container
+    this.head = head;
+    this.current = head;
+    this.next = function() {
+        // get the next container with respect to the current container
+        if (this.current.nextNode === undefined) {
+            this.current = this.head;
+            return undefined;
+        } else {
+            this.current = this.current.nextNode;
+            return this.current;
+        }
+    };
+}
