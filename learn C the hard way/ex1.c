55fc2dc16ecf9b80f1098deb7f223a7fca12bcee
sooham <rafizsooham@gmail.com>
2015-12-23 22:40:13 -0500

    Added first three lCthw exercises

---
+++ learn C the hard way/ex1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+    puts("Say my name");
+    puts("Walter!");
+    puts("You're goddamn right");
+    return 0;
+}
---
+++ learn C the hard way/ex3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int age = 20;
+    int height = 179;
+
+    printf("I am %d years old.\n", age);
+    printf("I am %d centimeters tall.\n", height);
+
+    return 0;
+}
